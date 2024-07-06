fe060c09d544a69aeee128d91cae4d569da25ec0
puppyl <928927619@qq.com>
2024-07-06 23:58:07 +0800

    Add ST7789 support

---
+++ MDK-ARM/LVGL/fonts.d
@@ -0,0 +1,3 @@
+lvgl\fonts.o: ..\Hardware\ST7789\fonts.c
+lvgl\fonts.o: ..\Hardware\ST7789\fonts.h
+lvgl\fonts.o: D:\MDK\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MDK-ARM/LVGL/startup_stm32h743xx.d
@@ -0,0 +1,1 @@
+lvgl\startup_stm32h743xx.o: startup_stm32h743xx.s
